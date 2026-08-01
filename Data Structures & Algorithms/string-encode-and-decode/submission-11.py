c60a0f755ba7cd7c24e7aa1d0d08f89a6ced08ed
Naman Singh <103762464+NamanSingh02@users.noreply.github.com>
2026-08-01 19:08:36 -0400

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        ans=''
+        for s in strs:
+            ans+=s
+            ans+="naman"
+        return ans
+        #return "naman".join(strs)
+        #why can't we use this drectly?
+        # because it produced identical encoding for [], ['']
+        #Our method: [] ->"" ['']->"naman"
+
+    def decode(self, s: str) -> List[str]:
+        ans=s.split("naman")
+        ans.pop() #Since "naman" will always come in the end, 
+        #there will be an extra empty string in the end
+        return ans
